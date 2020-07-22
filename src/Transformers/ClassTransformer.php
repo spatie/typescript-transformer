90fee592ca0a3291d834f482372c4afe25eb3b7e
Ruben Van Assche <ruben@spatie.be>
2020-07-22 11:21:50 +0200

    wip

--- src/Transformers/ClassTransformer.php
+++ src/Transformers/ClassTransformer.php
@@ -21,22 +21,14 @@ abstract class ClassTransformer implements Transformer
         $properties = $this->resolveProperties($class);
 
         $properties = array_map(
-            fn (ReflectionProperty $property) => $this->resolveTypeDefinition($property, $missingSymbols),
+            fn(ReflectionProperty $property) => $this->resolveTypeDefinition($property, $missingSymbols),
             $properties
         );
-
-        $output = "export type {$name} = {" . PHP_EOL;
-
-        foreach ($properties as $property) {
-            $output .= "    {$property}" . PHP_EOL;
-        }
-
-        $output .= '}' . PHP_EOL;
 
         return Type::create(
             $class,
             $name,
-            $output,
+            $this->toTypescript($name, $properties),
             $missingSymbols
         );
     }
@@ -45,7 +37,7 @@ abstract class ClassTransformer implements Transformer
     {
         $properties = array_filter(
             $class->getProperties(ReflectionProperty::IS_PUBLIC),
-            fn (ReflectionProperty $property) => ! $property->isStatic()
+            fn(ReflectionProperty $property) => ! $property->isStatic()
         );
 
         return array_values($properties);
@@ -55,30 +47,38 @@ abstract class ClassTransformer implements Transformer
         ReflectionProperty $reflection,
         MissingSymbolsCollection $missingSymbolsCollection
     ): string {
-        $resolvePropertyTypesAction = new ResolvePropertyTypesAction(
-            $missingSymbolsCollection,
-            $this->getClassPropertyProcessors()
-        );
-
-        $types = $resolvePropertyTypesAction->execute(
-            $this->resolveClassProperty($reflection)
-        );
-
-        return "{$reflection->getName()} : " . implode(' | ', $types) . ';';
-    }
-
-    protected function resolveClassProperty(ReflectionProperty $reflection): ClassProperty
-    {
         $fieldValidator = FieldValidator::fromReflection($reflection);
 
         $types = $fieldValidator->isNullable
             ? array_merge($fieldValidator->allowedTypes, ['null'])
             : $fieldValidator->allowedTypes;
 
-        return ClassProperty::create(
+        $classProperty = ClassProperty::create(
             $reflection,
             $types,
             $fieldValidator->allowedArrayTypes
         );
+
+        $resolvePropertyTypesAction = new ResolvePropertyTypesAction(
+            $missingSymbolsCollection,
+            $this->getClassPropertyProcessors()
+        );
+
+        $types = $resolvePropertyTypesAction->execute($classProperty);
+
+        return "{$reflection->getName()} : " . implode(' | ', $types) . ';';
+    }
+
+    protected function toTypescript(string $name, array $properties): string
+    {
+        $output = "export type {$name} = {" . PHP_EOL;
+
+        foreach ($properties as $property) {
+            $output .= "    {$property}" . PHP_EOL;
+        }
+
+        $output .= '}' . PHP_EOL;
+
+        return $output;
     }
 }
