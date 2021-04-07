0c02878919e3b8c74b226e12f796d090c16fef8d
Ruben Van Assche <ruben@spatie.be>
2021-04-07 17:00:04 +0200

    Update dto collection type processor

--- src/TypeProcessors/DtoCollectionTypeProcessor.php
+++ src/TypeProcessors/DtoCollectionTypeProcessor.php
@@ -2,21 +2,17 @@
 
 namespace Spatie\TypeScriptTransformer\TypeProcessors;
 
-use phpDocumentor\Reflection\Fqsen;
 use phpDocumentor\Reflection\Type;
-use phpDocumentor\Reflection\TypeResolver;
 use phpDocumentor\Reflection\Types\Array_;
-use phpDocumentor\Reflection\Types\Nullable;
 use phpDocumentor\Reflection\Types\Object_;
 use ReflectionClass;
 use ReflectionMethod;
 use ReflectionParameter;
 use ReflectionProperty;
-use ReflectionType;
 use Spatie\DataTransferObject\DataTransferObjectCollection;
 use Spatie\TypeScriptTransformer\Structures\MissingSymbolsCollection;
-use Spatie\TypeScriptTransformer\Types\TypeScriptType;
 use Spatie\TypeScriptTransformer\Support\UnknownType;
+use Spatie\TypeScriptTransformer\TypeReflectors\TypeReflector;
 
 class DtoCollectionTypeProcessor implements TypeProcessor
 {
@@ -38,26 +34,11 @@ class DtoCollectionTypeProcessor implements TypeProcessor
                 return $type;
             }
 
-            return new Array_($this->resolveType($fqs));
+            $reflection = new ReflectionClass($fqs);
+
+            return new Array_(
+                TypeReflector::new($reflection->getMethod('current'))->reflect()
+            );
         });
-    }
-
-    private function resolveType(string $collectionClass): Type
-    {
-        $reflection = new ReflectionClass($collectionClass);
-
-        $returnType = $reflection->getMethod('current')->getReturnType();
-
-        if (empty($returnType)) {
-            return new TypeScriptType('any');
-        }
-
-        $type = $returnType->isBuiltin()
-            ? (new TypeResolver())->resolve($returnType->getName())
-            : new Object_(new Fqsen('\\' . $returnType->getName()));
-
-        return $returnType->allowsNull()
-            ? new Nullable($type)
-            : $type;
     }
 }
