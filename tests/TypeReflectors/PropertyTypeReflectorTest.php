0fcea96d6fb57d0ccfbcf97587da59712758062b
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-13 20:07:32 +0300

    refactor PropertyTypeReflectorTest

--- tests/TypeReflectors/PropertyTypeReflectorTest.php
+++ tests/TypeReflectors/PropertyTypeReflectorTest.php
@@ -1,115 +1,103 @@
 <?php
 
-namespace Spatie\TypeScriptTransformer\Tests\TypeReflectors;
-
-use PHPUnit\Framework\TestCase;
-use ReflectionProperty;
 use Spatie\TypeScriptTransformer\Attributes\LiteralTypeScriptType;
 use Spatie\TypeScriptTransformer\TypeReflectors\PropertyTypeReflector;
+use function PHPUnit\Framework\assertEquals;
 
-class PropertyTypeReflectorTest extends TestCase
-{
-    /** @test */
-    public function it_can_reflect_from_reflection()
-    {
-        $class = new class {
-            public int $p1;
-            public ?int $p2;
-            public int | float $p3;
-            public int | float | null $p4;
-            public $p5;
-        };
+it('can reflect from reflection', function () {
+    $class = new class {
+        public int $p1;
+        public ?int $p2;
+        public int | float $p3;
+        public int | float | null $p4;
+        public $p5;
+    };
 
-        $this->assertEquals(
-            'int',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p1'))->reflect()
-        );
+    assertEquals(
+        'int',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p1'))->reflect()
+    );
 
-        $this->assertEquals(
-            '?int',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p2'))->reflect()
-        );
+    assertEquals(
+        '?int',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p2'))->reflect()
+    );
 
-        $this->assertEquals(
-            'int|float',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p3'))->reflect()
-        );
+    assertEquals(
+        'int|float',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p3'))->reflect()
+    );
 
-        $this->assertEquals(
-            'int|float|null',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p4'))->reflect()
-        );
+    assertEquals(
+        'int|float|null',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p4'))->reflect()
+    );
 
-        $this->assertEquals(
-            'any',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p5'))->reflect()
-        );
-    }
+    assertEquals(
+        'any',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p5'))->reflect()
+    );
+});
 
-    /** @test */
-    public function it_can_reflect_from_docblock()
-    {
-        $class = new class {
-            /** @var int */
-            public $p1;
+it('can reflect from docblock', function () {
+    $class = new class {
+        /** @var int */
+        public $p1;
 
-            /** @var ?int */
-            public $p2;
+        /** @var ?int */
+        public $p2;
 
-            /** @var int|float */
-            public $p3;
+        /** @var int|float */
+        public $p3;
 
-            /** @var int|float|null */
-            public $p4;
+        /** @var int|float|null */
+        public $p4;
 
-            public $p5;
+        public $p5;
 
-            /** @var array<array-key, string> */
-            public $p6;
-        };
+        /** @var array<array-key, string> */
+        public $p6;
+    };
 
-        $this->assertEquals(
-            'int',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p1'))->reflect()
-        );
+    assertEquals(
+        'int',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p1'))->reflect()
+    );
 
-        $this->assertEquals(
-            '?int',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p2'))->reflect()
-        );
+    assertEquals(
+        '?int',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p2'))->reflect()
+    );
 
-        $this->assertEquals(
-            'int|float',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p3'))->reflect()
-        );
+    assertEquals(
+        'int|float',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p3'))->reflect()
+    );
 
-        $this->assertEquals(
-            'int|float|null',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p4'))->reflect()
-        );
+    assertEquals(
+        'int|float|null',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p4'))->reflect()
+    );
 
-        $this->assertEquals(
-            'any',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p5'))->reflect()
-        );
+    assertEquals(
+        'any',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p5'))->reflect()
+    );
 
-        $this->assertEquals(
-            'array<array-key,string>',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p6'))->reflect()
-        );
-    }
+    assertEquals(
+        'array<array-key,string>',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p6'))->reflect()
+    );
+});
 
-    /** @test */
-    public function it_can_reflect_from_attribute()
-    {
-        $class = new class {
-            #[LiteralTypeScriptType('Integer')]
-            public $p1;
-        };
+it('can reflect from attribute', function () {
+    $class = new class {
+        #[LiteralTypeScriptType('Integer')]
+        public $p1;
+    };
 
-        $this->assertEquals(
-            'Integer',
-            (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p1'))->reflect()
-        );
-    }
-}
+    assertEquals(
+        'Integer',
+        (string) PropertyTypeReflector::create(new ReflectionProperty($class, 'p1'))->reflect()
+    );
+});
