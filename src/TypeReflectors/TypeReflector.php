e9dd67c6a9dd5d5a66fcb2a765e15b5691868a6f
Martin Gold <martin.gold@volny.cz>
2024-12-12 10:54:18 +0100

    feat: promoted property support

--- src/TypeReflectors/TypeReflector.php
+++ src/TypeReflectors/TypeReflector.php
@@ -36,6 +36,9 @@ abstract class TypeReflector
     public static function new(ReflectionMethod|ReflectionProperty|ReflectionParameter $reflection): static
     {
         if ($reflection instanceof ReflectionProperty) {
+            if ($reflection->isPromoted()) {
+                return new PromotedPropertyTypeReflector($reflection);
+            }
             return new PropertyTypeReflector($reflection);
         }
 
